7cb4bb388a9e4deabc8927ee0d7fdec17fee765a
neerajdubey2023 <neeraj.dubey06@gmail.com>
2023-07-25 19:11:50 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,12 @@
+
+ public class Test
+ {
+
+  public static void main(String args[])
+   {
+   System.out.println(" Starting Test");   
+    
+   System.out.println("\nThis is my First testing...!");   
+   }
+
+} 
